1006816eda6c68e9c94a135e40035465298d32a4
damithudayakumara <88715094+damithudayakumara@users.noreply.github.com>
2023-02-22 22:46:40 +0530

    Initial commit

---
+++ src/components/heading.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Heading() {
+  return <h1>hi</h1>;
+}
+export default Heading;
---
+++ src/components/list.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function List() {
+  return (
+    <ul>
+      <li>bacon</li>
+      <li>jacon</li>
+      <li>noodles</li>
+    </ul>
+  );
+}
+export default List;
